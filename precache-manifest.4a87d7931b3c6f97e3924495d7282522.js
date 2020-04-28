2081a66371a8f05494e56b652402c1e7a3e2a83c
traskiiin <taraskravets95@gmail.com>
2020-04-29 00:56:06 +0300

    Updates

---
+++ precache-manifest.4a87d7931b3c6f97e3924495d7282522.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31d74c15f62f55a483c3e2fa6643da94",
+    "url": "/react-cursor-3hw/index.html"
+  },
+  {
+    "revision": "000b10997386508c1375",
+    "url": "/react-cursor-3hw/static/css/main.2fd6805e.chunk.css"
+  },
+  {
+    "revision": "fd35e10b90a12b664d90",
+    "url": "/react-cursor-3hw/static/js/2.be408186.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-cursor-3hw/static/js/2.be408186.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "000b10997386508c1375",
+    "url": "/react-cursor-3hw/static/js/main.4cabcf82.chunk.js"
+  },
+  {
+    "revision": "aa548a4c5b3d96502597",
+    "url": "/react-cursor-3hw/static/js/runtime-main.182b4678.js"
+  }
+]);
